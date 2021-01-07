b1e39ccca7d695e32b9daa0836d28ff81b79fc8a
Sierra Wallace <sskwallace@gmail.com>
2021-01-07 21:58:30 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
-    add_column :grade integer, :birthdate string
+    def change
+    add_column  :grade, :integer
+    add_column  :birthdate, :string
   end
-end
 end
