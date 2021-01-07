7b525739bd8dbff628ae051e4b1f3256e2378d88
Sierra Wallace <sskwallace@gmail.com>
2021-01-07 22:08:34 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdates < ActiveRecord::Migration[5.2]
      def change
-    change_column(table_name, column_name, type) :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime
   end
 end
